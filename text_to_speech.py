e99bdb7925be5e75d76d2d795b60d4d3e8abee5d
Poornatejas.C <poorna1728@outlook.com>
2024-10-30 18:19:07 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,11 +1,11 @@
 # ----- Text to Speech ------- #
 
 
-'''from gtts import gTTS
+from gtts import gTTS
 from PyPDF2 import PdfReader
 import os
 
-f  =open('startup idea 1.pdf','rb')
+f  =open('text1.pdf','rb')
 pdf = PdfReader(f)
 text = ''
 for pages in pdf.pages:
@@ -15,8 +15,5 @@ language = 'en'
 
 audio = gTTS(text=text, lang=language,slow=False)
 audio.save('audio.wav')
-os.system('audio.wav')'''
+os.system('audio.wav')
 
-my_list = [x if x % 2 == 0 else -x for x in range(10)]
-print(my_list) 
-my_list = [x for x in range(10) if x % 2 == 0 else -x]
